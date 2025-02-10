4795a5a19d574a9705ba7dcee4196e6d59e218ec
Nick <wezawe@gmail.com>
2025-02-10 11:25:20 +0100

    populateKBFromCrawlerResults

--- src/scripts/populate_kb_from_crawler.ts
+++ src/scripts/populate_kb_from_crawler.ts
@@ -10,10 +10,17 @@ async function main(): Promise<void> {
             console.error('Please provide a bot ID');
             process.exit(1);
         }
-        const url =
-            'https://crawler.doghouse.agency/site/130/output/all?limit=500&page=1&filter%5Bresponse%5D=success&content%5Bis_job_type%5D=1';
+        const urls = [
+            'https://crawler.doghouse.agency/site/130/output/all?limit=500&page=1&filter%5Bresponse%5D=success&content%5Bis_job_type%5D=1',
+        ];
 
-        await populateKBFromCrawlerResults(botId, url);
+        for (const url of urls) {
+            try {
+                await populateKBFromCrawlerResults(botId, url);
+            } catch (error) {
+                console.error('Error:', error);
+            }
+        }
     } catch (error) {
         console.error('Error:', error);
         process.exit(1);
